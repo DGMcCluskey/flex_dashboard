ab79cc7cdea9664859ea9020e9360f6c2d788957
Dan McCluskey <d.mccluskey@ucl.ac.uk>
2026-03-12 11:56:42 +0000

    building up design

--- flex_app.py
+++ flex_app.py
@@ -11,15 +11,26 @@ metadata = list(adata.obs.columns)
 # UI
 app_ui = ui.page_fluid(
     ui.h2("scRNA-seq Explorer"),
-    ui.layout_sidebar(
-        ui.sidebar(
-            ui.input_selectize("gene", "Select gene", genes, multiple=False),
-            ui.input_selectize("groupby", "Group by metadata", metadata, multiple=False),
+    ui.card(
+        ui.layout_sidebar(
+            ui.sidebar(
+                ui.input_selectize("umap_gene", "Select gene", genes, multiple=False),
+                ui.input_selectize("umap_meta", "Group by metadata", metadata, multiple=False),
+            ),
+            ui.output_plot("umap_meta"),
+            ui.output_plot("umap_gene"),
+        )
+    ),
+    ui.card(
+        ui.layout_sidebar(
+            ui.sidebar(
+            ui.input_selectize("dotplot_gene", "Select gene", genes, multiple=False),
+            ui.input_selectize("dotplot_meta", "Group by metadata", metadata, multiple=False)
         ),
-        ui.output_plot("umap"),
         ui.output_image("dotplot"),  # dotplot displayed as image
-        ui.output_plot("violin"),
+        #ui.output_plot("violin"),
     )
+)
 )
 
 # Server
@@ -27,9 +38,16 @@ def server(input, output, session):
 
     @output
     @render.plot
-    def umap():
+    def umap_meta():
         fig, ax = plt.subplots()
-        sc.pl.umap(adata, color=input.gene(), ax=ax, show=False)
+        sc.pl.umap(adata, color=input.umap_meta(), ax=ax, show=False)
+        return fig
+
+    @output
+    @render.plot
+    def umap_gene():
+        fig, ax = plt.subplots()
+        sc.pl.umap(adata, color=input.umap_gene(), ax=ax, show=False)
         return fig
 
     @output
@@ -43,31 +61,17 @@ def server(input, output, session):
     # 2. Draw the Scanpy dotplot on that axis
         sc.pl.dotplot(
             adata,
-            var_names=[input.gene()],
-            groupby=input.groupby(),
+            var_names=[input.dotplot_gene()],
+            groupby=input.dotplot_meta(),
             ax=ax,
             show=False  # needed to draw without popping up
         )
-
     # 3. Save the figure to disk
         fig.savefig(tmp.name, bbox_inches="tight")
         plt.close(fig)
 
     # 4. Return dict with "src" because Shiny requires this format
         return {"src": tmp.name}
-        
-    @output
-    @render.plot
-    def violin():
-        fig, ax = plt.subplots()
-        sc.pl.violin(
-            adata,
-            keys=input.gene(),
-            groupby=input.groupby(),
-            ax=ax,
-            show=False
-        )
-        return fig
 
 # Run app
 app = App(app_ui, server)
